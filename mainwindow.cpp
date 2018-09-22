d8487bba20fb0789aa090afa228bd7983310db3c
Patryk <25955544+PatrykMar@users.noreply.github.com>
2018-09-22 21:26:18 +0100

    zork

---
+++ mainwindow.cpp
@@ -0,0 +1,49 @@
+#include <QInputDialog>
+#include <QLineEdit>
+#include <QCoreApplication>
+#include <QWidget>
+#include <QMainWindow>
+
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    //ciWin = new CharInfoWindow;
+    startButton = new QPushButton("Start", this);
+    startButton->setGeometry(QRect(QPoint(100, 100),QSize(200, 50)));
+
+    exitButton = new QPushButton("Exit", this);
+    exitButton->setGeometry(QRect(QPoint(100, 200),QSize(200, 50)));
+
+    scoreboard = new QPushButton("Scoreboard",this);
+    scoreboard->setGeometry(QRect(QPoint(100,200),QSize(200,50)));
+
+    connect(startButton, SIGNAL (clicked()), this, SLOT (startHandler()));
+    connect(exitButton, SIGNAL (clicked()), this, SLOT (exitHandler()));
+    connect(scoreboard,SIGNAL(clicked()), this,SLOT (scoreHandler()));
+}
+
+void MainWindow::startHandler()
+{
+    //ciWin->setFixedSize(400,400);
+    //ciWin->show();
+    close();
+}
+
+void MainWindow::exitHandler()
+{
+    QCoreApplication::quit();
+}
+
+void MainWindow::scoreHandler()
+{
+    close();
+    //Need to call scoreboard window here.
+}
+
+MainWindow::~MainWindow() {
+    delete startButton;
+    delete exitButton;
+   // delete ciWin;
+}
---
+++ mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void startHandler();
+    void exitHandler();
+    void scoreHandler();
+
+private:
+    QPushButton *startButton;
+    QPushButton *exitButton;
+    QPushButton *scoreboard;
+};
+
+#endif // MAINWINDOW_Hs
